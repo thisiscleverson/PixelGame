7a833fb8887ee21870209379e85923829bcb2cc2
cleverson <cleverson2020333@gmail.com>
2023-05-03 18:10:18 -0300

    added timer function

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import pygame, sys, time
 from pygame.locals import *
 from colors import Colors
 from random import randrange
+from datetime import datetime
 
 FPS       = 60
 WIDTH     = 750
@@ -18,6 +19,7 @@ class PixelGame:
       pygame.display.set_caption(caption)
       self.DISPLAYSURF = pygame.display.set_mode((WIDTH, HEIGHT))
       self.__fpsClock = pygame.time.Clock()
+      self.font = pygame.font.Font('./assets/fonts/montserrat.ttf',32)
 
 
       self.playerX = 0
@@ -26,11 +28,15 @@ class PixelGame:
       self.fruitX  = 0
       self.fruitY  = 0
 
+      self.point   = 0
+      self.seconds = self.__getSeconds()
+      self.time    = 10
+
       self.createGame() # Add a player and fruit in a random place
    
    def createGame(self) -> None:
       self.__addPlayer()
-      #self.__addFruit()
+      self.__addFruit()
 
    def __addPlayer(self) -> None:
       self.playerX = randrange(5, WIDTH  / 2)
@@ -50,7 +56,6 @@ class PixelGame:
    def __fruit(self,x, y) -> None:
       self.fruit = Rect(x, y, PIXELSIZE, PIXELSIZE) 
       pygame.draw.rect(self.DISPLAYSURF, colors.RED, self.fruit)
-
 
 
    def __moveUp(self, moveValue):
@@ -84,8 +89,32 @@ class PixelGame:
 
    def __checkForFruitCollision(self):
       if pygame.Rect.colliderect(self.player, self.fruit):
+         self.time    += 2
+         self.point   += 1
          self.__addFruit()
+
+   def __getSeconds(self) -> int:
+      return int(datetime.now().strftime('%S'))
+
+   def __timer(self,value) -> int: 
+      timer = self.time
+      if (self.__getSeconds() - self.seconds) > 1:
+         if timer - value >= 0:
+            timer -= value
+            self.time = timer      
+         self.seconds = self.__getSeconds()
+
+      return timer
    
+   def __printPoints(self):
+
+      textRender = self.font.render(f'Point: {self.point} | Time: {self.time}',True, colors.ORANGE) # text | antialias | color
+
+      textRender 
+      textRectObj = textRender.get_rect()
+      textRectObj.center = (WIDTH / 2,25) # set position 
+
+      self.DISPLAYSURF.blit(textRender, textRectObj)
 
    def run(self):
       while True:
@@ -102,6 +131,14 @@ class PixelGame:
          self.__fruit(x=self.fruitX, y=self.fruitY)
 
          self.__checkForFruitCollision()
+         
+         time = self.__timer(1)
+         #print(self.__timer(1) < 0)
+         if time == 0:
+            print('Game Over!')
+            break
+
+         self.__printPoints()
 
          pygame.display.update()
          self.__fpsClock.tick(FPS)
