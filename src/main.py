71c12cb74b598f7249c345cacb7a1903b4b86395
cleverson <cleverson2020333@gmail.com>
2023-05-04 18:03:24 -0300

    fix: timer bug fix

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import pygame, sys, time
 from pygame.locals import *
 from colors import Colors
 from random import randrange
-from datetime import datetime
+
 
 FPS       = 60
 WIDTH     = 750
@@ -18,9 +18,8 @@ class PixelGame:
       pygame.init()
       pygame.display.set_caption(caption)
       self.DISPLAYSURF = pygame.display.set_mode((WIDTH, HEIGHT))
-      self.__fpsClock = pygame.time.Clock()
-      self.font = pygame.font.Font('./assets/fonts/montserrat.ttf',32)
-
+      self.__fpsClock  = pygame.time.Clock()
+      self.font        = pygame.font.Font('./assets/fonts/montserrat.ttf',32)
 
       self.playerX = 0
       self.playerY = 0
@@ -28,93 +27,96 @@ class PixelGame:
       self.fruitX  = 0
       self.fruitY  = 0
 
+      #TODO: refatorar as variaveis de point, _milles, time
       self.point   = 0
-      self.seconds = self.__getSeconds()
-      self.time    = 10
+      self._milles = self.milles()
+      self.time    = 30
 
       self.createGame() # Add a player and fruit in a random place
    
    def createGame(self) -> None:
-      self.__addPlayer()
-      self.__addFruit()
+      self.addPlayer()
+      self.addFruit()
 
-   def __addPlayer(self) -> None:
+
+   def addPlayer(self) -> None:
       self.playerX = randrange(5, WIDTH  / 2)
       self.playerY = randrange(5, HEIGHT / 2)
-      self.__player(x=self.playerX, y=self.playerY)
+      self.player(x=self.playerX, y=self.playerY)
 
-   def __player(self,x, y) -> None:
-      self.player = Rect(x, y, PIXELSIZE, PIXELSIZE) 
-      pygame.draw.rect(self.DISPLAYSURF, colors.GREEN, self.player)   
-
+   def player(self,x, y) -> None:
+      self._player = Rect(x, y, PIXELSIZE, PIXELSIZE) 
+      pygame.draw.rect(self.DISPLAYSURF, colors.GREEN, self._player)   
    
-   def __addFruit(self) -> None:
+   def addFruit(self) -> None:
       self.fruitX = randrange(5, WIDTH  - FRUITSIZE)
       self.fruitY = randrange(5, HEIGHT - FRUITSIZE)
-      self.__player(x=self.fruitX, y=self.fruitY)
+      self.fruit(x=self.fruitX, y=self.fruitY)
 
-   def __fruit(self,x, y) -> None:
-      self.fruit = Rect(x, y, PIXELSIZE, PIXELSIZE) 
-      pygame.draw.rect(self.DISPLAYSURF, colors.RED, self.fruit)
+   def fruit(self,x, y) -> None: # TODO: refatorar
+      self._fruit = Rect(x, y, PIXELSIZE, PIXELSIZE) 
+      pygame.draw.rect(self.DISPLAYSURF, colors.RED, self._fruit)
 
 
-   def __moveUp(self, moveValue):
+   def moveUp(self, moveValue):
       if self.playerY - moveValue > 0:
          self.playerY -= moveValue
 
-   def __moveDown(self, moveValue):
+   def moveDown(self, moveValue):
       if self.playerY + moveValue < (HEIGHT - PIXELSIZE):
          self.playerY += moveValue
    
-   def __moveRight(self, moveValues):
+   def moveRight(self, moveValues):
       if self.playerX + moveValues  < (WIDTH - PIXELSIZE):
          self.playerX += moveValues
    
-   def __moveLeft(self, moveValues):
+   def moveLeft(self, moveValues):
       if self.playerX - moveValues > 0:
          self.playerX -= moveValues
 
-
-   def __movePlayer(self,event):
+   def movePlayer(self):
       keyPressed = pygame.key.get_pressed() # Listener keys of keyboard
 
       if keyPressed[pygame.K_LEFT]:
-         self.__moveLeft(MOVESPEED)
+         self.moveLeft(MOVESPEED)
       if keyPressed[pygame.K_RIGHT]:
-         self.__moveRight(MOVESPEED)
+         self.moveRight(MOVESPEED)
       if keyPressed[pygame.K_UP]:
-         self.__moveUp(MOVESPEED)
+         self.moveUp(MOVESPEED)
       if keyPressed[pygame.K_DOWN]:
-         self.__moveDown(MOVESPEED)
+         self.moveDown(MOVESPEED)
 
-   def __checkForFruitCollision(self):
-      if pygame.Rect.colliderect(self.player, self.fruit):
+
+   def checkForFruitCollision(self):
+      if pygame.Rect.colliderect(self._player, self._fruit):
          self.time    += 2
          self.point   += 1
-         self.__addFruit()
+         self.addFruit()
 
-   def __getSeconds(self) -> int:
-      return int(datetime.now().strftime('%S'))
 
-   def __timer(self,value) -> int: 
-      timer = self.time
-      if (self.__getSeconds() - self.seconds) > 1:
-         if timer - value >= 0:
-            timer -= value
-            self.time = timer      
-         self.seconds = self.__getSeconds()
+   def milles(self) -> int:
+      return int(round(time.time() * 1000))
 
-      return timer
+   #TODO: refatorar o timer
+   def timer(self,value) -> int: 
+      time = self.time
+      if (self.milles() - self._milles) > 1000: 
+         if time - value >= 0:
+            time -= value
+            self.time = time     
+         self._milles = self.milles()
+
+      return time
    
-   def __printPoints(self):
-
+   
+   def showPointAndTime(self):
       textRender = self.font.render(f'Point: {self.point} | Time: {self.time}',True, colors.ORANGE) # text | antialias | color
 
-      textRender 
       textRectObj = textRender.get_rect()
-      textRectObj.center = (WIDTH / 2,25) # set position 
+      textRectObj.center = ((WIDTH / 2), 25) # set position 
 
       self.DISPLAYSURF.blit(textRender, textRectObj)
+
 
    def run(self):
       while True:
@@ -125,20 +127,21 @@ class PixelGame:
                pygame.quit()
                sys.exit()
 
-         self.__movePlayer(event)
+         self.movePlayer()
 
-         self.__player(x=self.playerX, y=self.playerY)
-         self.__fruit(x=self.fruitX, y=self.fruitY)
+         self.player( x=self.playerX,  y=self.playerY)
+         self.fruit(  x=self.fruitX,   y=self.fruitY)
 
-         self.__checkForFruitCollision()
+         self.checkForFruitCollision()
          
-         time = self.__timer(1)
-         #print(self.__timer(1) < 0)
+         #refatorar o time
+         time = self.timer(1)
+
          if time == 0:
             print('Game Over!')
             break
 
-         self.__printPoints()
+         self.showPointAndTime()
 
          pygame.display.update()
          self.__fpsClock.tick(FPS)
